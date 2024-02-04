a1d560e6ced153c11587d3b35761a4de39fd6489
andcr <fatxullohibrohim@gmail.com>
2024-02-04 22:36:42 +0500

    test12

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    shopcard = models.IntegerField(null=True)
     def __str__(self):
         return self.name
     def save_customer(self):
